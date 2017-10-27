3f6212d6c96419962ffcc6ada9c84155f4ba0591
negus <nfatah41@gmail.com>
2017-10-27 12:14:40 +0300

    fix apollo 2.0 requires inMemory Cache

--- components/index.js
+++ components/index.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from 'react-apollo';
 import Router from './router'
 
-const App = () => <Router />
+class App extends Component {
+  createClient() {
+    // Initialize Apollo Client with URL to our server
+    return new ApolloClient({
+    link: new HttpLink({ uri: 'https://api.github.com/graphql' }),
+    cache: new InMemoryCache(),
+    })
+  }
+  render () {
+    return (
+      <ApolloProvider client={this.createClient()}>
+        <Router />
+      </ApolloProvider>
+    )
+  }
+}
 
 export default App
