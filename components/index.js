ebd2e0f89087a1b2f3d383443cdd14d8a6bfd2d2
negus <nfatah41@gmail.com>
2017-10-27 12:48:55 +0300

    add authentication middleware to apollo client

--- components/index.js
+++ components/index.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react'
 import ApolloClient from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo';
 import Router from './router'
 
 class App extends Component {
   createClient() {
+    const httpLink = createHttpLink({ uri: 'https://api.github.com/graphql'})
+    const middleareLink =  new ApolloLink((operation, forward) => {
+      operation.setContext({
+        headers: {
+          authorization: 'GITHUB_ACCESS_TOKEN'
+        }
+      })
+      return forward(operation)
+    })
+    const link = middleareLink.concat(httpLink)
+
     // Initialize Apollo Client with URL to our server
     return new ApolloClient({
-    link: new HttpLink({ uri: 'https://api.github.com/graphql' }),
+    link: link,
     cache: new InMemoryCache(),
     })
   }
