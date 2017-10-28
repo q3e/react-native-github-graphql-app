4dc459c2dcc1c39cfb091458651c1900cbde172d
negus <nfatah41@gmail.com>
2017-10-28 13:39:30 +0300

    update apollo client  - add github access token to query header

--- components/index.js
+++ components/index.js
@@ -4,20 +4,34 @@ import { ApolloLink } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo';
+import { onError } from 'apollo-link-error';
 import Router from './router'
+import { GITHUB_ACCESS_TOKEN } from '../config'
 
 class App extends Component {
   createClient() {
     const httpLink = createHttpLink({ uri: 'https://api.github.com/graphql'})
-    const middleareLink =  new ApolloLink((operation, forward) => {
+
+    // handle network error
+    const errorLink = onError(({ networkError }) => {
+        if (networkError.statusCode === 401) {
+          console.log(networkError)
+        }
+      // let errorMessage = networkError.statusCode === 401 ? 'Network error 104, handled' : 'link sucess'
+      // console.log(errorMessage, networkError)
+    })
+
+    // apply widdleware to add access token to request
+    let middlewareLink =  new ApolloLink((operation, forward) => {
       operation.setContext({
         headers: {
-          authorization: 'GITHUB_ACCESS_TOKEN'
+        authorization : `Bearer ${GITHUB_ACCESS_TOKEN}`
         }
       })
       return forward(operation)
     })
-    const link = middleareLink.concat(httpLink)
+    const link = middlewareLink.concat(httpLink)
+    
 
     // Initialize Apollo Client with URL to our server
     return new ApolloClient({
