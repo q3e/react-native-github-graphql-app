4f71f7ff7337dc3f32cecddfd8158ce99d6ebe71
negus <nfatah41@gmail.com>
2017-10-28 13:36:43 +0300

    add error handling + add graphql query + apollo HoC wrapper

--- components/stateless/screens/ReposList.js
+++ components/stateless/screens/ReposList.js
@@ -1,28 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native';
-// import { graphql } from 'react-apollo';
-// import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 // The data prop, which is provided by the wrapper below contains,
 // a `loading` key while the query is in flight and posts when ready
-export default ReposList = ({ data }) => {
+const ReposList = ({ data }) => {
   console.log(data)
-  return <Text> {data}</Text>
+  if(!data.error){
+    if(data.loading){
+      return <Text>fetching posts... </Text>
+    }else return <Text> fetching post sucess</Text>
+  }else <Text> Error Fetching posts</Text>
 }
 
-// // The `graphql` wrapper executes a GraphQL query and makes the results
-// // available on the `data` prop of the wrapped component (PostList here)
-// export default graphql(gql`{
-//   search(type: REPOSITORY, query: "react", first: 20) {
-//     edges {
-//       node {
-//         ... on Repository {
-//           nameWithOwner
-//           owner {
-//             login
-//           }
-//         }
-//       }
-//     }
-//   }
-// }`, { options: { notifyOnNetworkStatusChange: true } })(ReposList);
+// The `graphql` wrapper executes a GraphQL query and makes the results
+// available on the `data` prop of the wrapped component (PostList here)
+export default graphql(gql`{
+  search(type: REPOSITORY, query: "react", first: 20) {
+    edges {
+      node {
+        ... on Repository {
+          nameWithOwner
+          owner {
+            login
+          }
+        }
+      }
+    }
+  }
+}`, { options: { notifyOnNetworkStatusChange: true } })(ReposList);
