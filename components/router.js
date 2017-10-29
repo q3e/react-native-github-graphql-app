5ca751dbb2aff72c55c4d8c41966c72fe80e46f2
negus <nfatah41@gmail.com>
2017-10-29 13:46:59 +0300

    Fix prop returning class instead of value of prop

--- components/router.js
+++ components/router.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { StackNavigator } from 'react-navigation'
 import Home from './stateless/screens/Home'
 import ReposList from './stateless/screens/ReposList'
-// import SEARCH_QUERY from './stateless/screens/Home'
+import { SEARCH_QUERY } from './stateless/screens/Home'
 
 const Router = StackNavigator({
   Home: { screen: Home },
-  ReposList: { screen: props => <ReposList {...props} searchQuery="react" /> }
+  ReposList: { screen: props => <ReposList {...props} searchQuery={SEARCH_QUERY} /> }
   // ContributorsList: { ContributorsList }
 })
 
