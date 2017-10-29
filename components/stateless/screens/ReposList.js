f1b6fa352aa001995f6fdc9a24cd43733bd8dd47
negus <nfatah41@gmail.com>
2017-10-29 18:17:27 +0300

    update gql query with variable

--- components/stateless/screens/ReposList.js
+++ components/stateless/screens/ReposList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Text, FlatList  } from 'react-native';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { SEARCH_QUERY } from './Home'
 
 // The data prop, which is provided by the wrapper below contains,
 // a `loading` key while the query is in flight and posts when ready
@@ -11,7 +12,7 @@ const ReposList = ({ data: { loading, error, search }, searchQuery}) => {
       return <Text>fetching posts... </Text>
     }else {
       const responseData = search.edges
-      console.log(searchQuery)
+      console.log('You are searching for', SEARCH_QUERY) // did we need to pass props :(
       return (
         <FlatList
           data={responseData}
@@ -22,19 +23,46 @@ const ReposList = ({ data: { loading, error, search }, searchQuery}) => {
   }else <Text> Error Fetching posts</Text>
 }
 
-// The `graphql` wrapper executes a GraphQL query and makes the results
-// available on the `data` prop of the wrapped component (ReposList here)
-export default graphql(gql`{
-  search(type: REPOSITORY, query: "react", first: 20) {
-    edges {
-      node {
-        ... on Repository {
-          nameWithOwner
-          owner {
-            login
+
+const searchRepos = gql`
+  query searchRepos($query: String!) {
+    search(type: REPOSITORY, query: $query, first: 100) {
+      edges {
+        node {
+          ... on Repository {
+            nameWithOwner
+            owner {
+              login
+            }
           }
         }
       }
     }
   }
-}`, { options: { notifyOnNetworkStatusChange: true } })(ReposList);
+`
+
+// const searchRepos = gql`{
+//   search(type: REPOSITORY, query: "react", first: 100) {
+//       edges {
+//         node {
+//           ... on Repository {
+//             nameWithOwner
+//             owner {
+//               login
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
+
+// The `graphql` wrapper executes a GraphQL query and makes the results
+// available on the `data` prop of the wrapped component (ReposList here)
+export default graphql(searchRepos, {
+  options: {
+    variables: { query: SEARCH_QUERY }, 
+    notifyOnNetworkStatusChange: true
+  }
+})(ReposList);
+
