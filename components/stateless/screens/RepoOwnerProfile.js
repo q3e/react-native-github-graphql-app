76dc0892d3743f95ce34d00eeaeddb048848cf2e
negus <nfatah41@gmail.com>
2017-10-31 08:31:12 +0300

    add content+scrollview + remove org query

--- components/stateless/screens/RepoOwnerProfile.js
+++ components/stateless/screens/RepoOwnerProfile.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import gql from 'graphql-tag'
 import { graphql } from 'react-apollo'
 
 const RepoOwnerProfile = ({
+  // data: { loading, error, networkStatus, user: {
+  //   name, bio, avatarUrl, repositories, followers, following, starredRepositories
+  //   }
+  // },
   data,
   navigation: {
     state: {
@@ -11,20 +15,45 @@ const RepoOwnerProfile = ({
     }
   }
 }) => {
-console.log(data)
-return <View><Text> Profile @{username}</Text></View>
-
+  if (!data.error) {
+    if (data.loading) {
+      return <Text>fetching github user...</Text>
+    } else {
+      const { name, bio, avatarUrl, repositories, followers, following, starredRepositories} = data.user
+      console.log(data)
+      return (
+        <ScrollView>
+          <View style={{ flex: 1}}>
+            <Image
+              style={{flex: 2}}
+              source={{uri: avatarUrl}}
+            />
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+              <View style={{flex: 1}}>
+                <Text>Bio: {bio}</Text>
+              </View>
+              <View style={{ flex: 1}}>
+                  <Text>Total Repos: {repositories.totalCount}</Text>
+              </View>
+              <View style={{ flex: 1}}>
+                <Text>starred Repos: {starredRepositories.totalCount}</Text>
+              </View>
+              <View style={{ flex: 1}}>
+                <Text>followers: {followers.totalCount}</Text>
+              </View>
+              <View style={{ flex: 1}}>
+                <Text>following: {following.totalCount}</Text>
+              </View>
+              <Text> Profile @{username}</Text>
+            </View>
+          </View>
+        </ScrollView>
+       )
+    }
+  }
 }
-
 const getUserProfile = gql`
   query getUserProfile($login: String!){
-    organization(login: $login){
-      name
-      repositories{
-        totalCount
-      }
-      avatarUrl
-    }
     user(login: $login) {
       name
       bio
